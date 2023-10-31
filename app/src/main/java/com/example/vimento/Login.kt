77ba27085213a5123ebe7b9a199243f677bdee95
Felip1205 <andresfelipe.marroquinmartin@gmail.com>
2023-10-30 23:26:37 -0500

    primera parte inicio sesion

--- app/src/main/java/com/example/vimento/Login.kt
+++ app/src/main/java/com/example/vimento/Login.kt
@@ -3,21 +3,19 @@ package com.example.vimento
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.vimento.databinding.ActivityLoginBinding
 import com.example.vimento.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener() {
-            startActivity(Intent(this, Login::class.java))
-        }
-        binding.textView3.setOnClickListener() {
+        binding.button7.setOnClickListener() {
             startActivity(Intent(this, Registrarse::class.java))
         }
     }
--- app/src/main/java/com/example/vimento/Registrarse.kt
+++ app/src/main/java/com/example/vimento/Registrarse.kt
@@ -4,21 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vimento.databinding.ActivityMainBinding
+import com.example.vimento.databinding.ActivityRegistrarseBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class Registrarse : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrarseBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityRegistrarseBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener() {
+        binding.button6.setOnClickListener() {
             startActivity(Intent(this, Login::class.java))
-        }
-        binding.textView3.setOnClickListener() {
-            startActivity(Intent(this, Registrarse::class.java))
         }
     }
 }
