1225684afc2088908ba8feacf20cc7804588b0d3
Felip1205 <andresfelipe.marroquinmartin@gmail.com>
2023-10-31 22:10:11 -0500

    actividades yy otros

---
+++ app/src/main/java/com/example/vimento/IngresarNota.kt
@@ -0,0 +1,11 @@
+package com.example.vimento
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IngresarNota : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingresar_nota)
+    }
+}
